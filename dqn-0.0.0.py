50a930544dfe78a0cf184850559087f95faf3e6d
Juan David Valencia <judvalenciaan@unal.edu.co>
2019-01-21 16:28:46 -0500

    Adding replay memory and playing with the image

---
+++ dqn-0.0.0.py
@@ -0,0 +1,43 @@
+# Let's play randomly
+
+from collections import namedtuple
+import gym
+import time
+import matplotlib.pyplot as plt
+env = gym.make("Centipede-v0")
+observation = env.reset()
+
+# Making replay memory structure
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+            self.capacity = capacity
+            self.memory = []
+            self.position = 0
+
+    def push(self, *args):
+        ''' Saves a transition.'''
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+    
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+#for _ in range(1000):
+print(env.observation_space)
+screen = env.render(mode='rgb_array')
+# From position 215 to 250, is the point board
+plt.imshow(screen[:215])
+plt.show()
+time.sleep(0.05)
+action = env.action_space.sample()
+observation, reward, done, info = env.step(action)
+
+
