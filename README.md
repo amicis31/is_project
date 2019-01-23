6f308ed05dee4607e10c1e9716aa390474987d16
Juan David Valencia <judvalenciaan@unal.edu.co>
2019-01-23 08:14:21 -0500

    Adding necessary functions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # is_project
 Intelligent systems project DQN vs DDPG
+
+http://www.cs.toronto.edu/~tijmen/csc321/slides/lecture_slides_lec6.pdf
--- dqn-0.0.0.py
+++ dqn-0.0.0.py
@@ -4,6 +4,14 @@ from collections import namedtuple
 import gym
 import time
 import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.transforms as T
+import random
+import math
+
 env = gym.make("Centipede-v0")
 observation = env.reset()
 
@@ -30,14 +38,142 @@ class ReplayMemory(object):
         return len(self.memory)
 
 
+# Basic neural net
+class DQN(nn.Module):
+    def __init__(self, h, w):
+        super(DQN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+        self.bn3 = nn.BatchNorm2d(32)
+
+        def conv2d_size_out(size, kernel_size=5, stride=2):
+            return (size - (kernel_size - 1) - 1) // stride + 1
+
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
+
+        linear_input_size = convw * convh * 32
+
+        self.head = nn.Linear(linear_input_size, 18)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        return self.head(x.view(x.size(0), -1))
+
+    
+
+
+
+
+
+
 #for _ in range(1000):
 print(env.observation_space)
 screen = env.render(mode='rgb_array')
 # From position 215 to 250, is the point board
-plt.imshow(screen[:215])
+screen = torch.tensor(screen)
+screen = screen[:215, :, :]
+plt.imshow(screen)
 plt.show()
 time.sleep(0.05)
 action = env.action_space.sample()
 observation, reward, done, info = env.step(action)
 
 
+# Getting the image of the game
+def get_screen():
+    '''This function returns the process image for the network'''
+    screen = env.render(mode='rgb_array')
+    screen = torch.tensor(screen)
+    screen = screen[:215, :, :]
+    return screen
+
+
+# Setting action selection variables
+BATCH_SIZE = 128
+GAMMA = 0.999
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
+TARGET_UPDATE = 10
+
+
+# Setting networks for DQN and algorithm
+init_screen = get_screen()
+screen_height, screen_width, _ = init_screen.shape
+
+policy_net = DQN(screen_height, screen_width).to(device)
+target_net = DQN(screen_height, screen_width).to(device)
+target_net.load_state_dict(policy_net.state_dict())
+target_net.eval()
+
+optmizer = optim.RMSprop(policy_net.parameters)
+memory = ReplayMemory(10000)
+
+steps_done = 0
+
+def select_action(state):
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            return policy_net(state).max(1)[1].view(1, 1)
+    else:
+        return torch.tensor([[random.randrange(2)]], device=device, dtype=torch.long)
+
+
+def optimize_model():
+    if len(memory) < BATCH_SIZE:    
+        return
+    transition = memory.sample(BATCH_SIZE)
+
+    batch = Transition(*zip(*transition))
+
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=device, dtype=torch.uint8)
+    
+
+## Training loop
+## TODO: Change form of state
+num_episodes = 0
+for i_episode in range(num_episodes):
+    env.reset()    
+    screen = get_screen()
+    state = screen # Why?
+    for t in count():
+        # Select and perform an action
+        action = select_action(state)
+        _, reward, done, _ = env.step(action.item())
+        reward = torch.tensor([reward], device=device)
+
+        # Observe new state
+        last_screen = current_screen
+        current_screen = get_screen()
+        if not done:
+            next_state = current_screen - last_screen
+        else:
+            next_state = None
+
+        # Store the transition in memory
+        memory.push(state, action, next_state, reward)
+
+        # Move to the next state
+        state = next_state
+
+        # Perform one step of the optimization (on the target network)
+        optimize_model()
+        if done:
+            episode_durations.append(t + 1)
+            break
+
+        if i_episode % TARGET_UPDATE == 0:
+            target_net.load_state_dict(policy_net.state_dict())
+
+    print('Complete')
